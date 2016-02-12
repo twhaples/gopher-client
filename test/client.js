37e622524c9fa246e8e52a5f9b9c0685397602df
Thomas Whaples <fennec@gmail.com>
2016-02-12 14:03:05 -0500

    Minimal Gopher client.
    A simple .get method provides an array of objects to its callback.
    Types are not properly recognized yet and there's still plenty of
    poorly-defined behavior but it passes a happy use-case successfully.

---
+++ test/client.js
@@ -0,0 +1,44 @@
+'use strict';
+var Gopher = require('../index.js');
+var Server = require('gopher-node');
+var assert = require('chai').assert;
+
+var host = '127.0.0.1';
+var port = 7770;
+
+var testApp = Server.createServer(function(req, resp) {
+  resp.write(['0Loopback', 'example', host, port].join('\t'));
+  resp.write('\r\n.\r\n');
+  resp.end();
+});
+
+describe('gopher.client', function() {
+  before(function(done) {
+    testApp.listen(port, function(error){
+      done(error);
+    });
+  });
+  after(function(done) {
+    // XXX this is a hack because gopher-node doesn't expose the method
+    testApp.netServer.close(done);
+  });
+
+
+  describe("get", function() {
+    it("should query a host for data", function(done){
+      Gopher.get(host,port,'example', function(error, menu) {
+        assert.equal(error, undefined);
+        assert.typeOf(menu, 'array');
+      
+        var entry = menu[0];
+        assert.instanceOf(entry, Gopher.MenuEntry);
+        assert.equal(entry.text(), 'Loopback');
+        assert.equal(entry.path(), 'example');
+        assert.equal(entry.host(), host);
+        assert.equal(entry.port(), port);
+        done();
+      });
+    });
+  });
+});
+
