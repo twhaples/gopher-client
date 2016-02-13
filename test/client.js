d530949d02ffd6ab69a4b8f9223c0983806ef006
Thomas Whaples <fennec@gmail.com>
2016-02-12 21:53:49 -0500

    Make everything pretty

--- test/client.js
+++ test/client.js
@@ -3,48 +3,48 @@ var Server = require('gopher-node');
 var assert = require('chai').assert;
 
 var gopher = require('../index.js');
-var DirectoryEntry = require('../lib/directory-entry')
+var DirectoryEntry = require('../lib/directory-entry');
 
 var host = '127.0.0.1';
 var port = 7770;
 
-var testApp = Server.createServer(function(req, resp) {
+var testApp = Server.createServer(function (req, resp) {
   resp.write(['1Loopback', 'example'].join('\t'));
   resp.write(['', host, port].join('\t'));
   resp.write('\r\n.\r\n');
   resp.end();
 });
 
-describe('gopher.client', function() {
-  before(function(done) {
-    testApp.listen(port, function(error){
+describe('gopher.client', function () {
+  before(function (done) {
+    testApp.listen(port, function (error) {
       done(error);
     });
   });
-  after(function(done) {
+
+  after(function (done) {
     // XXX this is a hack because gopher-node doesn't expose the method
     testApp.netServer.close(done);
   });
 
-
-  describe('getDirectory', function() {
-    it('should query a host for data', function(done){
+  describe('getDirectory', function () {
+    it('should query a host for data', function (done) {
       var client = new gopher.Client();
-      client.getDirectory(host,port,'example', function(error, Directory) {
+      client.getDirectory(host, port, 'example', function (error, directory) {
         assert.equal(error, undefined);
-        assert.deepEqual(Directory, [
+        assert.deepEqual(directory, [
           new DirectoryEntry('1', 'Loopback', 'example', host, port)
         ]);
         done();
       });
     });
 
-    it('should return the system error when it cannot connect', function(done) {
+    it('should return the system error when it cannot connect', function (done) {
       var client = new gopher.Client();
-      client.getDirectory(host,port+1, 'example', function(error, Directory) {
+      client.getDirectory(host, port + 1, 'example', function (error, directory) {
         assert.ok(error);
         assert.match(error.message, /connect ECONNREFUSED/);
-        assert.deepEqual(Directory, []);
+        assert.deepEqual(directory, []);
         done();
       });
     });
--- test/parser.js
+++ test/parser.js
@@ -1,47 +1,55 @@
 'use strict';
 var assert = require('chai').assert;
-var Parser = require('../lib/parser')
-var DirectoryEntry = require('../lib/directory-entry')
+var Parser = require('../lib/parser');
+var DirectoryEntry = require('../lib/directory-entry');
 
-describe('Parser', function(){
-  describe('.parseLine', function(){
-    describe('on an ordinary Directory line', function(){
+describe('Parser', function () {
+  describe('.parseLine', function () {
+    describe('on an ordinary Directory line', function () {
       var parsed;
-      before(function(){
+      before(function () {
         parsed = new Parser().parseLine('1Hello!\ttest\texample.net\t7070');
-      })
-      it('should return a DirectoryEntry item', function(){
-        assert.deepEqual(parsed, new DirectoryEntry(
-          '1','Hello!','test','example.net',7070)
+      });
+
+      it('should return a DirectoryEntry item', function () {
+        assert.deepEqual(
+          parsed,
+          new DirectoryEntry('1', 'Hello!', 'test', 'example.net', 7070)
         );
       });
-      it('should get .text() right', function(){
+
+      it('should get .text() right', function () {
         assert.equal(parsed.text(), 'Hello!');
       });
-      it('should get .uri() right', function() {
+
+      it('should get .uri() right', function () {
         assert.equal(parsed.uri(), 'gopher://example.net:7070/1test');
       });
     });
 
-    it('can parse informational lines (with dummy links)', function(){
+    it('can parse informational lines (with dummy links)', function () {
       var parsed = new Parser().parseLine('iinfo\tfake\t(null))\t0\tgopher+++');
       assert.deepEqual(parsed, new DirectoryEntry('i', 'info', null, null, null));
     });
 
-    it('can parse textfile links', function(){
-      var parsed = new Parser().parseLine('0The good stuff!\tgoodstuff.txt\texample.org\t70');
-      assert.deepEqual(parsed, new DirectoryEntry(
-        '0','The good stuff!','goodstuff.txt','example.org',70));
+    it('can parse textfile links', function () {
+      var parsed = new Parser().parseLine(
+        '0The good stuff!\tgoodstuff.txt\texample.org\t70'
+      );
+      assert.deepEqual(
+        parsed,
+        new DirectoryEntry('0', 'The good stuff!', 'goodstuff.txt', 'example.org', 70)
+      );
     });
 
-    it('should ignore extra data at the end', function(){
+    it('should ignore extra data at the end', function () {
       var parsed = new Parser().parseLine('1x\ty\texample.net\t70\tgopher+++');
-      assert.deepEqual(parsed, new DirectoryEntry('1','x','y','example.net',70));
+      assert.deepEqual(parsed, new DirectoryEntry('1', 'x', 'y', 'example.net', 70));
     });
 
-    describe('on unparseable lines', function(){
-      var parse = function(line){ return new Parser().parseLine(line); };
-      it('should return null when something cannot be parsed', function(){
+    describe('on unparseable lines', function () {
+      var parse = function (line) { return new Parser().parseLine(line); };
+      it('should return null when something cannot be parsed', function () {
         assert.equal(parse(''), null, 'the empty string');
         assert.equal(parse('.'), null, 'the dot at the end of a gopher list');
         assert.equal(parse('0hello\tworld'), null, 'a line that is too short');
